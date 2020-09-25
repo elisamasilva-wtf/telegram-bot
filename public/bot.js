4e185b4a1af23b48a7a56989c9d95cfff6a00b37
André Walker <awalker-wtf@wtf.inf.br>
2020-09-25 15:09:20 -0300

    faz funcioanr o envio de mensagem do bot para o cliente

--- public/bot.js
+++ public/bot.js
@@ -1,8 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const op = require('./myaction.js');
 // Substitua o valor abaixo pelo token de telegrama que você recebe de @BotFather
-const token = '1297260365:AAGmjpv-tz1TvqvaPTGZi-byRU_xtR6iqaw';
-
+// const token = '1297260365:AAGmjpv-tz1TvqvaPTGZi-byRU_xtR6iqaw';
+const token = '1350778571:AAE1tLVc3roqisqipFj1B_ZVISPfXZEuUlE';
+var chatId = '';
 // Crie um bot que usa 'polling' para buscar novas atualizações
 const bot = new TelegramBot(token, { polling: true });
 
@@ -15,11 +16,13 @@ bot.on('message', (msg) => {
   const start = new Date();
   console.log('Data: ', start.toLocaleString());
   //console.log('AQUI', msg);
-  const chatId = msg.chat.id;
+  chatId = msg.chat.id;
   const newuser = msg.chat.first_name;
   const newmsg = msg.text;
   console.log('Usuário: ', newuser);
   console.log('Mensagem Recebida: ', newmsg);
+  console.log("OP: ", op);
+  
 
   //textContent = Define ou recupera o conteúdo de texto de um nó e qualquer nó filho.
   msgReceived.textContent = newmsg;
@@ -34,12 +37,12 @@ bot.on('message', (msg) => {
   //responseMsg(msg);
 });
 
-function responseMsg(msg) {
+function responseMsg() {
   //const text = msg.text;
-  const chatId = msg.chat.id;
+  
   //const text = 'Estou funcionando';
   var text = document.getElementById('sendMsg').value;
-  //bot.sendMessage(chatId, text);
+  bot.sendMessage(chatId, text);
 
   console.log('Resposta:', text);
   console.log('id:', chatId);
@@ -58,3 +61,6 @@ bot.on('edited_message', (msg) => {
     'Atenção! Ao editar a mensagem esta alteração não é replicada ao operador, por favor envie uma nova mensagem!'
   );
 });
+
+sendButton = document.getElementById("send-button").addEventListener("click", responseMsg);
+
